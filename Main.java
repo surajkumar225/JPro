1151d1413978195e36fc973924ed6908d6eab61f
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-08-05 23:14:04 +0530

    Strt

--- Main.java
+++ Main.java
@@ -4,7 +4,8 @@ public class Main{
     public static void main(String[] arg){
         System.out.println("Hey Trinity!");
         Scanner input = new Scanner(System.in);
-        System.out.println(input.next());
-        // System.out.println(input.nextInt());
+        System.out.println(input.next());   // To print a string
+        // System.out.println(input.nextInt());  // To print integer
+        // System.out.println(input.nextLine());  // To print a line
     }
 }
