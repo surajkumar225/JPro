1f8c99138b1d46cb4774a4831cc63f59cbabce0d
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-08-07 22:57:24 +0530

    Strt

--- Main.java
+++ Main.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] arg){
-        System.out.println("Hey Trinity!");
-        Scanner input = new Scanner(System.in);
+
+        
+        // System.out.println("Hey Trinity!");
+        // Scanner input = new Scanner(System.in);
         // System.out.println(input.next());   // To print a string
-        System.out.println(input.nextInt());  // To print integer
+        // System.out.println(input.nextInt());  // To print integer
         // System.out.println(input.nextLine());  // To print a line
     }
 }
