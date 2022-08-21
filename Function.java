2256a3b77db479082f85bc5cc9978f9bf9d3588a
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-08-21 21:39:34 +0530

    Functions

--- Function.java
+++ Function.java
@@ -2,21 +2,28 @@ import java.util.Scanner;
 
 public class Function{
     public static void main(String[] arg){
-        int ans = sum(20,30);
-        System.out.print("The sum is : " + ans);
+        // int ans = sum(20,30);
+        // System.out.print("The sum is : " + ans);
 
         // String check = myGreet("Dawson");
         // System.out.print(check);
 
+        int[] arr = {1, 2, 5, 9, 21};
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+
        
     }
-    static int sum(int a , int b){
-            int sum = a + b;
-            return sum;
-        }
+    // static int sum(int a , int b){
+    //         int sum = a + b;
+    //         return sum;
+    //     }
 
     // static String myGreet(String name){
     //     String message = "Hey " + name;
     //     return message;
+
+    static void change(int[] num){
+        num[0] = 77;
     }
 }
