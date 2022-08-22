acd2b649b5058b48d5c5a961cc3d07bf6c4b7f89
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-08-22 21:14:11 +0530

    PrimeorNot

---
+++ PrimeorN.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PrimeorN{
+    public static void main(String[] arg){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        boolean ans = isPrime(n);
+        System.out.println(ans);
+    }
+    static boolean isPrime(int n){
+        if(n <= 1){
+            return false;
+        }
+        int c = 2;
+        while(c * c <= n){
+            if(n % c == 0){
+                return false;
+            }
+            c++;
+        }
+        if(c * c > n){
+            return true;
+        }
+        return false;
+
+
+    }
+}
