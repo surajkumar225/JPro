bb32d476323f615e6edd93e047a360b56d869cec
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-08-24 23:58:37 +0530

    Arrays

--- Array.java
+++ Array.java
@@ -10,20 +10,32 @@ public class Array{
         // String[] word = new String[5];
         // System.out.println(word[2]);
 
-        int[] arr = new int[2];
+        // int[] arr = new int[2];
         // arr[0] = 45;
         // arr[1] = 54;
         // System.out.print(arr[1]);
 
-        for(int i=0; i<arr.length; i++){
-            arr[i] = in.nextInt();
-        }
+        // for(int i=0; i<arr.length; i++){
+        //     arr[i] = in.nextInt();
+        // }
         // for(int i=0; i<arr.length; i++){
         //    System.out.print(arr[i] + " ");
         // }
 
-        for(int num : arr){
-            System.out.print(num + " ");
+        // for(int num : arr){
+        //     System.out.print(num + " ");
+        // }
+
+        String[] str = new String[4];
+        for(int i = 0; i< str.length; i++){
+            str[i] = in.next();
         }
+        System.out.println(Array.toString(str));
+
+        //Modify
+        str[1]= "Surk";
+        System.out.println(Array.toString(str));
+
+
     }
 }
