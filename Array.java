9a22730c0f119209ea9caf0a0d8806e3a69b7acf
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-08-24 23:37:36 +0530

    Arrays

---
+++ Array.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Array{
+    public static void main(String[] arg){
+        // int[] ros;
+        // ros = new int[5];
+        // System.out.println(ros[2]);
+
+        String[] word = new String[5];
+        System.out.println(word[2]);
+    }
+}
