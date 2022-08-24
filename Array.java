464110d83216b385b6825fa446b5a660ef34ab79
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-08-24 23:51:27 +0530

    Arrays

--- Array.java
+++ Array.java
@@ -2,11 +2,28 @@ import java.util.Scanner;
 
 public class Array{
     public static void main(String[] arg){
+        Scanner in = new Scanner(System.in);
         // int[] ros;
         // ros = new int[5];
         // System.out.println(ros[2]);
 
-        String[] word = new String[5];
-        System.out.println(word[2]);
+        // String[] word = new String[5];
+        // System.out.println(word[2]);
+
+        int[] arr = new int[2];
+        // arr[0] = 45;
+        // arr[1] = 54;
+        // System.out.print(arr[1]);
+
+        for(int i=0; i<arr.length; i++){
+            arr[i] = in.nextInt();
+        }
+        // for(int i=0; i<arr.length; i++){
+        //    System.out.print(arr[i] + " ");
+        // }
+
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
     }
 }
