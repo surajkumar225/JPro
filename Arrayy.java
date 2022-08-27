b05580661c3fe49a8c16319992c596d3aca4e2f4
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-08-27 21:44:18 +0530

    Arrayy

--- Arrayy.java
+++ Arrayy.java
@@ -3,13 +3,26 @@ import java.util.Arrays;
 
 public class Arrayy{
     public static void main(String[] arg){
-        int[] nums = {3, 4, 5, 6};
-        System.out.println(Arrays.toString(nums));
-        change(nums);
-        System.out.println(Arrays.toString(nums));
+        // int[] nums = {3, 4, 5, 6};
+        // System.out.println(Arrays.toString(nums));
+        // change(nums);
+        // System.out.println(Arrays.toString(nums));
+
+    int[][] arr = {
+        {1, 2, 3, 4},
+        {5, 6},
+        {7, 8, 9}
+    };
+
+    for (int row = 0; row < arr.length; row++) {
+        for (int col = 0; col < arr[row].length; col++) {
+            System.out.print(arr[row][col] + " ");
+        }
+        System.out.println();
         
 }
-static void change(int[] arr){
-    arr[0] = 99;
+// static void change(int[] arr){
+//     arr[0] = 99;
+// }
 }
 }
