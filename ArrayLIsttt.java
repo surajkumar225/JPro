49ce81a7a22f0e55eb5d4fcaabc118c4945e5cc5
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-08-28 23:45:31 +0530

    ArrayList

---
+++ ArrayLIsttt.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListtt{
+    public static void main(String[] arg){
+        ArrayList<Integer> list = new ArrayList<>(10);
+
+        list.add(67);
+        list.add(497);
+        list.add(603);
+        list.add(345);
+        list.add(927); 
+        list.add(61);
+        list.add(787);
+        list.add(391);
+        list.add(4902);
+        list.add(65);
+
+        System.out.println(list.contains(765432));
+        System.out.println(list);
+    }
+}
