ebb0da8e144c6d39e17c68969e52bbf65826e4cf
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-08-31 22:57:55 +0530

    ArrayList

--- ArrayLIsttt.java
+++ ArrayLIsttt.java
@@ -3,23 +3,33 @@ import java.util.Scanner;
 
 public class ArrayListtt{
     public static void main(String[] arg){
+        Scanner in = new Scanner(System.in);
         ArrayList<Integer> list = new ArrayList<>(10);
 
-        list.add(67);
-        list.add(497);
-        list.add(603);
-        list.add(345);
-        list.add(927); 
-        list.add(61);
-        list.add(787);
-        list.add(391);
-        list.add(4902);
-        list.add(65);
+        // list.add(67);
+        // list.add(497);
+        // list.add(603);
+        // list.add(345);
+        // list.add(927); 
+        // list.add(61);
+        // list.add(787);
+        // list.add(391);
+        // list.add(4902);
+        // list.add(65);
 
-        System.out.println(list.contains(76543210));
-        System.out.println(list);
+        // System.out.println(list.contains(76543210));
+        // System.out.println(list);
         
-        list.set(0, 99);
+        // list.set(0, 99);
+        // System.out.println(list);
+
+        for(int i = 0; i < 5; i++){
+            list.add(in.nextInt());
+        }
+
+        for(int i = 0; i < 5; i++){
+            System.out.println(list.get(i));
+        }
         System.out.println(list);
     }
 }
