a23a07080b67cdbb824650d807ebcc855c0e3b76
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-09-05 23:42:57 +0530

    Swap

--- SwapArray.java
+++ SwapArray.java
@@ -11,13 +11,13 @@ public class SwapArray {
     static void reverse(int[] arr) {
         int start = 0;
         int end = arr.length-1;
-
         while (start < end) {
             swap(arr, start, end);
             start++;
             end--;
         }
     }
+    
     static void swap(int[] arr, int index1, int index2) {
         int temp = arr[index1];
         arr[index1] = arr[index2];
