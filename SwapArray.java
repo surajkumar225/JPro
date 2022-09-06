ac996e63db40d3d992d2a6e7e73cdefc090c3682
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-09-06 23:52:59 +0530

    Swap

--- SwapArray.java
+++ SwapArray.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class SwapArray {
     public static void main(String[] args) {
         int[] arr = {1, 3, 23, 9, 18, 56};
-        // swap(arr, 0, 4);
+        swap(arr, 0, 4);
         reverse(arr);
         System.out.println(Arrays.toString(arr));
     }
@@ -18,8 +18,6 @@ public class SwapArray {
         }
     }
 
-
-    
     static void swap(int[] arr, int index1, int index2) {
         int temp = arr[index1];
         arr[index1] = arr[index2];
